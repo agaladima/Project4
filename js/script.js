44c884e2f99e8ccf06503bbacb201a446265beed
Arum <arum.galadima@gmail.com>
2017-08-31 00:15:46 -0400

    middleish
    middleish

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-console.log('project4');
-console.log('get ready');
+//Hide current page
+$('.board').hide();
+
+//Show the startup screen
+const html = '<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><a href="#" class="button">Start game</a></header></div>';
+$('body').append(html);
+
+//Show the board when the button is clicked and hide the startup screen
+const showBoard = () => {
+	$('#start').hide();
+	$('.board').show();
+};
+$('.button').click(showBoard);
+
+//make player1 default to active
+$('#player1').addClass('active');
+
+//do something when mouseover happens
+$('.boxes').mouseover(function() {
+	console.log('mouseover');
+});
